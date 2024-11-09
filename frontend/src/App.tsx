259e9c7ccbc957253f0f01188d7cd9b5d0a62f6b
Gal Buki <gal.buki@iu-study.org>
2024-11-09 21:34:11 +0200

    initial react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-blue-600 grid h-[100dvh] items-center justify-center text-center">
+        <div>
+          <h1 className="text-4xl font-bold">Hello world!</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
