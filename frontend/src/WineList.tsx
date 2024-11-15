e8713b6df827daf73c7c71f8520759b8e78c853f
Gal Buki <gal.buki@iu-study.org>
2024-11-15 19:52:09 +0200

    move rating stars into its own component

--- frontend/src/WineList.tsx
+++ frontend/src/WineList.tsx
@@ -4,13 +4,14 @@ import { wineEntry } from "./types";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import RatingStarts from "./RatingStars";
 
 const WineList = ( { rating }: { rating: string }) => {
     // keep the wine list in this app state
     const [wines, setWines] = useState<wineEntry[]>([]);
 
     // sort order app state (0: no order, 1: ascending, 2: descending)
-    const [sortOrder, setSortOrder] = useState<Number>(0);
+    const [sortOrder, setSortOrder] = useState<number>(0);
 
     // will be called after pages' first render or rating or sortOrder is updated
     useEffect(() => {
@@ -84,25 +85,7 @@ const WineList = ( { rating }: { rating: string }) => {
                     <div className="flex justify-center w-1/4">{wine.name}</div>
                     <div className="flex justify-center w-1/4">{wine.year}</div>
                     <div className="flex justify-center w-1/4">
-                        {/* use 5 stars to display rating */}
-                        <div className="flex items-center">
-                            {/* change css for star color fill based on wine rating */}
-                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 1 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                            </svg>
-                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 2 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                            </svg>
-                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 3 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                            </svg>
-                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 4 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                            </svg>
-                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 5 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                            </svg>
-                        </div>
+                        <RatingStarts rating={wine.rating} />
                     </div>
                     <div className="flex justify-center w-1/4" key={wine.id}></div>
                 </div>
