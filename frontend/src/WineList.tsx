2254ec7b3f9b32ff06a1dad8dff8111fc3f62fc2
Gal Buki <gal.buki@iu-study.org>
2024-11-15 16:30:38 +0200

    use stars to display rating

--- frontend/src/WineList.tsx
+++ frontend/src/WineList.tsx
@@ -44,7 +44,27 @@ export const WineList = ( { rating }: { rating: string }) => {
                 <div className="flex min-h-full items-center">
                     <div className="flex justify-center w-1/4">{wine.name}</div>
                     <div className="flex justify-center w-1/4">{wine.year}</div>
-                    <div className="flex justify-center w-1/4">{wine.rating}</div>
+                    <div className="flex justify-center w-1/4">
+                        {/* use 5 stars to display rating */}
+                        <div className="flex items-center">
+                            {/* change css for star color fill based on wine rating */}
+                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 1 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                            </svg>
+                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 2 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                            </svg>
+                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 3 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                            </svg>
+                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 4 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                            </svg>
+                            <svg className={"w-4 h-4 ms-1" + (wine.rating >= 5 ? " text-yellow-300" : " text-gray-300")} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                            </svg>
+                        </div>
+                    </div>
                     <div className="flex justify-center w-1/4" key={wine.id}></div>
                 </div>
             </div>
